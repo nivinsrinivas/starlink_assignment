36d4948304a03fd3dcdd74203157b93242bb36bb
Nivin Srinivas <nivinsrinivas@gmail.com>
2024-02-10 19:26:38 +0530

    Add shell script

--- elt_pipeline/transform/models/tap-spacexapi/overview.md
+++ elt_pipeline/transform/models/tap-spacexapi/overview.md
@@ -9,13 +9,22 @@ This dbt project focuses on modeling data related to SpaceX's Starlink satellite
 - **Description:** The `starlink` table contains information about launches of Starlink satellites until 2022. This data serves as the primary source for this dbt project.
 
 ## Models
-### Starlink Prediction
 - **Description:** The `starlink_prediction` model is responsible for cleaning and transforming the Starlink data. It prepares the data in a format that can be used to make predictions about future satellite launches. This model is essential for forecasting and analyzing trends in Starlink satellite deployments.
 
 ## Documentation
 - **Tests:** Implemented
 - **Description:** The Starlink Prediction model has been thoroughly documented to provide insights into its purpose, usage, and underlying logic. Additionally, tests have been implemented to ensure the accuracy and reliability of the model's transformations.
 
+## Prediction
+### Historical Data 
+- Number of SpaceX launches of starlink satellites, so far: 68
+- Number of satellites launched so far: 5297
+
+### Prediction by when SpaceX will reach 42000 satellites with number of launches it will take:
+- Estimated number of launches needed to reach 42,000 Starlink satellites: 471
+- The date by when Spacex will launch 42000 Starlink satellites is predicted to be approximately: 2049-12-30!!
+
+The above prediction is made using the historical data of Starlink launches in the SpaceX API. 
 For more detailed information about SpaceX API, 
-Read: https://github.com/r-spacex/SpaceX-API/blob/master/docs/README.md
+- Read: https://github.com/r-spacex/SpaceX-API/blob/master/docs/README.md
 {% enddocs %}
--- starlink_prediction/prediction.py
+++ starlink_prediction/prediction.py
@@ -18,7 +18,7 @@ schema = 'analytics'
 table_name = 'starlink_prediction'
 
 # Name of the output CSV file
-output_csv_file = 'starlink_prediction.csv'
+starlink_data_csv = 'starlink_prediction.csv'
 
 
 try:
@@ -49,63 +49,94 @@ conn.close()
 
 
 # Write the data to a CSV file
-with open(output_csv_file, 'w', newline='') as csv_file:
+with open(starlink_data_csv, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow([desc[0] for desc in cursor.description])  # Write column headers
     csv_writer.writerows(rows)  # Write rows of data
 
-print(f"\nData from table '{table_name}' exported to '{output_csv_file}'")
+print(f"\nData from table '{table_name}' exported to '{starlink_data_csv}'\n")
 
 
 # Silence pandas warnings
 pd.options.mode.chained_assignment = None
 
-# Load the data to a dataframe
-starlink_data_df = pd.read_csv('starlink_prediction.csv')
+
+def get_starlink_data():
+    """Process CSV data, filter NULLs"""
+    # Load the data to a dataframe
+    starlink_data_df = pd.read_csv(starlink_data_csv)
+
+    # Convert launch_date to datetime
+    starlink_data_df['launch_date'] = pd.to_datetime(starlink_data_df['launch_date'])
+    
+    starlink_data_df['satellite_name'] = starlink_data_df['satellite_name'].fillna('0')
+    starlink_data_df['satellite_number'] = starlink_data_df['satellite_name'].str.extract(r'(\d+)').astype(float).astype(pd.Int64Dtype())
 
 
-starlink_data_df['satellite_name'] = starlink_data_df['satellite_name'].fillna('0')
-starlink_data_df['satellite_number'] = starlink_data_df['satellite_name'].str.extract(r'(\d+)').astype(float).astype(pd.Int64Dtype())
+    # Filter null data
+    starlink_data_filtered_df = starlink_data_df[starlink_data_df['satellite_name'].str.contains('STARLINK', na=False)]
 
-# Filter null data
-starlink_data_filtered_df = starlink_data_df[starlink_data_df['satellite_name'].str.contains('STARLINK', na=False)]
+    # Drop rows with NaN values in the satellite_number column
+    starlink_data_filtered_df.dropna(subset=['satellite_number'], inplace=True)
 
+    # Sort DataFrame by launch_date
+    starlink_data_filtered_df.loc[:, 'launch_date'] = pd.to_datetime(starlink_data_filtered_df['launch_date'])    
 
-# Find number of satellites launched so far
-latest_satellite_count = int(starlink_data_filtered_df['satellite_number'].max())
+    return [starlink_data_df, starlink_data_filtered_df]
 
-print("\nNumber of satellites launched so far:", latest_satellite_count)
+def predict_target_date():
+    """Function to predict date by when 42000 satellites will be launched"""
 
+    starlink_data_filtered_df = get_starlink_data()[1]
 
-# Predicting when will Spacex Launch 42000 Starlink satellites ?!
+    # Predicting when will Spacex Launch 42000 Starlink satellites ?!
+    # Create a new DataFrame with only launch_date and satellite_number
+    df_filtered = starlink_data_filtered_df[['launch_date', 'satellite_number']]
 
-# Convert launch_date to datetime
-starlink_data_filtered_df['launch_date'] = pd.to_datetime(starlink_data_filtered_df['launch_date'])
+    # Create features (X) and target variable (y)
+    X = df_filtered['launch_date'].astype(int).values.reshape(-1, 1)  # Convert launch_date to integer for regression
+    y = df_filtered['satellite_number'].values
 
-# Drop rows with NaN values in the satellite_number column
-starlink_data_filtered_df.dropna(subset=['satellite_number'], inplace=True)
+    # Fit linear regression model
+    model = LinearRegression()
+    model.fit(X, y)
 
+    # Predict satellite_number for the date when it reaches 42000
+    date_42000 = (42000 - model.intercept_) / model.coef_[0]
+    # Convert to seconds
+    date = str(datetime.utcfromtimestamp(date_42000 / 1e9).date())
 
-# Sort DataFrame by launch_date
-starlink_data_filtered_df.loc[:, 'launch_date'] = pd.to_datetime(starlink_data_filtered_df['launch_date'])
-
-# Create a new DataFrame with only launch_date and satellite_number
-df_filtered = starlink_data_filtered_df[['launch_date', 'satellite_number']]
-
-# Create features (X) and target variable (y)
-X = df_filtered['launch_date'].astype(int).values.reshape(-1, 1)  # Convert launch_date to integer for regression
-y = df_filtered['satellite_number'].values
-
-# Fit linear regression model
-model = LinearRegression()
-model.fit(X, y)
-
-# Predict satellite_number for the date when it reaches 42000
-date_42000 = (42000 - model.intercept_) / model.coef_[0]
-# Convert to seconds
-date = str(datetime.utcfromtimestamp(date_42000 / 1e9).date())
-
-print(f"\nThe date by when Spacex will launch 42000 Starlink satellites is predicted to be approximately: {date} !!!\n")
+    print(f"\nThe date by when Spacex will launch 42000 Starlink satellites is predicted to be approximately: {date}!!\n")
 
 
 
+def predict_number_of_launches():
+    """Get the number of launches it will take to reach 42000 satellites"""
+    # Fetch distinct count of launch_dates
+    starlink_data_unfiltered_df = get_starlink_data()[0]
+    distinct_launch_count = starlink_data_unfiltered_df['launch_date'].nunique()
+    
+    print("Number of SpaceX launches of starlink satellites, so far:", distinct_launch_count)
+    
+    starlink_data_filtered_df = get_starlink_data()[1]
+
+    # Find number of satellites launched so far
+    latest_satellite_count = int(starlink_data_filtered_df['satellite_number'].max())
+
+    print("\nNumber of satellites launched so far:", latest_satellite_count)
+
+    # Calculate the average number of satellites launched per launch
+    average_satellites_per_launch = latest_satellite_count / distinct_launch_count
+
+    # Estimate the number of launches needed to reach 42,000 satellites
+    estimated_launches_needed = (42000 - latest_satellite_count) / average_satellites_per_launch
+
+    # Round up to the nearest integer
+    estimated_launches_needed = int(estimated_launches_needed + 0.5)
+
+    print("\nEstimated number of launches needed to reach 42,000 Starlink satellites:", estimated_launches_needed)
+
+
+
+predict_number_of_launches()
+predict_target_date()
